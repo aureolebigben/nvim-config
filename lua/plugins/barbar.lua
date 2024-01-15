3a7f90354759c5aacdc2978b82517d8217e20d69
aureolebigben <olivier@ciss.fr>
2024-01-15 17:47:15 +0100

    added offset for neo-tree on barbar

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -1,11 +1,24 @@
 return {
-  "romgrk/barbar.nvim",
-  dependencies = {
-    "lewis6991/gitsigns.nvim",   -- OPTIONAL: for git status
-    "nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
-  },
-  init = function()
-    vim.g.barbar_auto_setup = false
-  end,
-  version = "^1.0.0",
+	"romgrk/barbar.nvim",
+	dependencies = {
+		"lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
+		"nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
+	},
+	init = function()
+		vim.g.barbar_auto_setup = false
+	end,
+	version = "^1.0.0",
+	config = function()
+		require("barbar").setup({
+			animation = true,
+			clickable = true,
+			tab_pages = false,
+			auto_hide = 1,
+			focus_on_close = "left",
+			highligh_visible = true,
+			sidebar_filetypes = {
+				["neo-tree"] = { event = "BufWipeout" },
+			},
+		})
+	end,
 }
