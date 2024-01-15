f299a3c52750e7c0ad6ed8f27087076cfe4b6a47
aureolebigben <olivier@ciss.fr>
2024-01-15 10:14:42 +0100

    add barbar plugin

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,6 +4,12 @@ return {
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
+    require("catppuccin").setup({
+      transparent_background = false,
+      integrations = {
+        notify = true
+      }
+    })
 		vim.cmd.colorscheme("catppuccin")
 	end,
 }
