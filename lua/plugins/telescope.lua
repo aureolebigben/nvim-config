779d3b392ee47354a2460aed3c148b703c201fa7
aureolebigben <olivier@ciss.fr>
2024-01-17 10:53:00 +0100

    added telescope-fzf-native and show search hidden files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,15 +4,39 @@ return {
 		tag = "0.1.5",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
+			require("telescope").setup({
+				pickers = {
+					find_files = {
+						hidden = true,
+					},
+				},
+			})
+
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<C-p>", builtin.find_files, {
 				desc = "Find files",
 			})
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, {
-        desc = "Find files"
-      })
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, {
+				desc = "Find files",
+			})
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {
 				desc = "Find in files",
+			})
+		end,
+	},
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "make",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					fzf = {
+						fuzzy = true,
+						override_generic_sorter = true,
+						override_file_sorter = true,
+						case_mode = "ignore_case",
+					},
+				},
 			})
 		end,
 	},
@@ -41,11 +65,11 @@ return {
 			})
 			require("telescope").load_extension("undo")
 
-      local undo = require("telescope").extensions.undo
+			local undo = require("telescope").extensions.undo
 
-      vim.keymap.set("n", "<leader>u", undo.undo, {
-        desc = "Undo"
-      })
+			vim.keymap.set("n", "<leader>u", undo.undo, {
+				desc = "Undo",
+			})
 		end,
 	},
 }
