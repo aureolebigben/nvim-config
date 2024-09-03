13c1a6cd900287ca8f22cde0a3f9c47557233ac3
aureolebigben <olivier.jarry@ciss.fr>
2024-09-03 12:28:01 +0200

    ajout dépendance nvim-dap-ui

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -16,6 +16,7 @@ return {
 		"rcarriga/nvim-dap-ui",
 		dependencies = {
 			"mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio"
 		},
 		config = function()
 			local dap, dapui = require("dap"), require("dapui")
